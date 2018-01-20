5bc62221a74eb63c1e3cd23bf45fe7b3189b15c1
Yinghao Wang <yinghao.a.wang@gmail.com>
2018-01-19 17:51:20 -0800

    upload to aws working

--- server/routes/video.js
+++ server/routes/video.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var async = require('async');
 var request = require('request');
+var async = require('async');
+var fs = require('fs');
+var filePath = ('./file.mp4');
+var stream = require('stream')
 
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
@@ -12,7 +15,45 @@ var BUCKET_NAME = 'yourvideostreamer';
 
 var bucketParams = {
     Bucket: BUCKET_NAME,
+    Key: 'file.mp4',
 };
+
+/*
+function uploadFromStream(s3) {
+    var pass = new stream.PassThrough();
+    var params = bucketParams;
+    params['Body'] = pass;
+    s3.upload(params, function(err, data){
+        console.log(err, data);
+    });
+    return pass;
+}
+*/
+
+router.post('/', function(req, res, next) {
+    async.parallel([
+        function(callback) {
+            fs.stat(filePath, function(err, stats) {
+                if (err) callback(err);
+                else {
+                    var file = fs.createReadStream(filePath);
+                    //file.pipe(uploadFromStream(s3));
+                    var params = bucketParams;
+                    params['Body'] = file;
+                    params['ContentType'] = 'video/mp4';
+                    s3.upload(params, function(err, data) {
+                        if (err) callback(err);
+                        else callback(null, data);
+                    });
+                }
+            });
+        },
+    ],
+    function(err, results) {
+        if (err) res.send(err);
+        else res.send(results);
+    });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -28,10 +69,10 @@ router.get('/', function(req, res, next) {
     ],
     function(err, results) {
         if (err) {
-            res.send(500, "Server error");
+            res.status(500).send(err);
         }
-        var contents = results[0]['Contents'];
-        res.send(JSON.stringify(contents));
+        //var contents = results[0]['Contents'];
+        res.send(JSON.stringify(results));
     });
 });
 router.get('/:id', function(req, res, next) {
