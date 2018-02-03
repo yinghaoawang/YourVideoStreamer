ebcb7fe5ce35f65ff57ce7368751cf04915ca375
Yinghao Wang <yinghao.a.wang@gmail.com>
2018-02-02 16:33:51 -0800

    env name change

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ var mysql = require('mysql2/promise');
 var config = require('./config');
 var pool = mysql.createPool({
     connectionLimit: 20,
-    host: config.mysql.host || process.env.SQL_HOST || 'localhost',
-    user: config.mysql.user || process.env.SQL_USER || 'root',
-    password: config.mysql.password || process.env.SQL_PASSWORD || '',
-    database: config.mysql.database || process.env.SQL_DATABASE || 'yourvideostreamer'
+    host: config.mysql.host || process.env.MYSQL_HOST || 'localhost',
+    user: config.mysql.user || process.env.MYSQL_USER || 'root',
+    password: config.mysql.password || process.env.MYSQL_PASSWORD || '',
+    database: config.mysql.database || process.env.MYSQL_DATABASE || 'yourvideostreamer'
 });
 
 module.exports = pool;
